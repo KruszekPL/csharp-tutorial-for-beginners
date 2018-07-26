1b547dce74266119ea02e3523c5b768e560e265a
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-07-26 09:43:48 +0200

    Section 6, Lecture 56, Exercise 2

---
+++ S6_L56_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace S6_L56_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var name = Console.ReadLine();
+
+            var charName = name.ToCharArray();
+            Array.Reverse(charName);
+
+            var reversedName = new String(charName);
+
+            Console.WriteLine("Reversed name: {0}", reversedName);
+        }
+    }
+}
