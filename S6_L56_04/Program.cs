951f2bf8987da26cb35005665640acafdee1628d
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-07-26 11:39:25 +0200

    Section 6, Lecture 56, Exercise 4

---
+++ S6_L56_04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S6_L56_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers (Quit to end):");
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                if (input.ToLower().Equals("quit"))
+                {
+                    break;
+                }
+
+                var number = int.Parse(input);
+                if (!numbers.Contains(number))
+                {
+                numbers.Add(number);
+                }
+            }
+
+            var result = String.Join(',', numbers);
+            Console.WriteLine("Your numbers are: {0}", result);
+        }
+    }
+}
