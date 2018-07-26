19d09c10b5fb396312f6c462a35f51e247cd4f75
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-07-26 11:04:42 +0200

    Section 6, Lecture 56, Exercise 3

---
+++ S6_L56_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S6_L56_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            while (numbers.Count < 5)
+            {
+                Console.Write("Enter number: ");
+                var input = int.Parse(Console.ReadLine());
+                
+                if (numbers.Contains(input))
+                {
+                    Console.WriteLine("Error! This number already exists...");
+                    continue;
+                }
+
+                numbers.Add(input);
+            }
+
+            numbers.Sort();
+
+            var sortedNumbers = String.Join(',', numbers);
+
+            Console.WriteLine("Your numbers are: {0}.", sortedNumbers);
+        }
+    }
+}
