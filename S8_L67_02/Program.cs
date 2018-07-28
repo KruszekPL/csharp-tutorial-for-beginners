34151bba1f6316567b9a843f112cc135fba1eeee
Marcin Kruszynski (Windows) <github@kruszynscy.pl>
2018-07-28 12:23:21 +0200

    Section 8, Lecture 67, Exercise 2

---
+++ S8_L67_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace S8_L67_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few numbers separated by a hyphen: ");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return;
+            }
+
+            var numbers = input
+                            .Split('-')
+                            .Select(Int32.Parse)
+                            .ToArray();
+            Array.Sort(numbers);
+            var duplicates = false;
+
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] == numbers[i])
+                {
+                    duplicates = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine(duplicates ? "Duplicates" : "No Duplicates");
+        }
+    }
+}
